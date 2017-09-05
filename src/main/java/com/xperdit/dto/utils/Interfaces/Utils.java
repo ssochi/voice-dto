03c8120898f83a81e7c2ecbeeb9590e3dc43044a
Wqlshoci <408711072@qq.com>
2017-09-05 20:27:48 +0800

    Add method toString to interface Utils .

--- src/main/java/com/xperdit/dto/utils/Interfaces/Utils.java
+++ src/main/java/com/xperdit/dto/utils/Interfaces/Utils.java
@@ -1,4 +1,9 @@
 package com.xperdit.dto.utils.Interfaces;
 
+import com.xperdit.dto.annotations.DtoListener;
+import com.xperdit.dto.utils.proxyListener.ToStringListener;
+
 public interface Utils extends Mapper, JsonDeserializer, JsonSerializer,Clear {
+    @DtoListener(DtoListener = ToStringListener.class)
+    public String toString();
 }
